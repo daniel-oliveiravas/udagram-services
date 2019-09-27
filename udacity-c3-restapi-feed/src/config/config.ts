ae1c25362d62a925cfe233de1ecd9fc344b4e3b9
Daniel Vasconcelos <daniel.oliveiravas@gmail.com>
2019-09-26 21:45:38 -0300

    fix: use correct image name in k8s deployments

--- udacity-c3-restapi-feed/src/config/config.ts
+++ udacity-c3-restapi-feed/src/config/config.ts
@@ -8,14 +8,17 @@ export const config = {
     "aws_reigion": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_BUCKET,
-    "url": process.env.URL    
+    "url": process.env.URL
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DB,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_media_bucket": process.env.AWS_BUCKET,
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
   },
   "jwt": {
     "secret": process.env.JWT_SECRET
